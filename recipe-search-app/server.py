b9abe92ac72ec327242f2c3e9f8fccb6ea7e2b8e
keptizefyrai <85554489+keptizefyrai@users.noreply.github.com>
2023-11-05 10:16:39 +0000

    Update server.py

--- recipe-search-app/server.py
+++ recipe-search-app/server.py
@@ -8,27 +8,33 @@ CORS(app)  # This will enable CORS for all routes
 # Load your CSV data into a DataFrame
 df = pd.read_csv('./data/food_ingredients_and_allergens.csv')
 
+
 @app.route('/search_recipes', methods=['GET'])
 def search_recipes():
-    query = request.args.get('query', '')  # Get the search query from URL parameters
-    # Implement a search functionality based on your CSV structure and the query
-    # For now, let's just filter rows that contain the query string in a 'recipe_name' column
-    filtered_recipes = df[df['Food Product'].str.contains(query, case=False, na=False)]
+    query = request.args.get('query', '')
+    allergen_to_check = request.args.get('allergen', '')
 
-    # Calculate allergens and percentages here
-    # This is a placeholder example where 'allergens' is a column in your CSV
-    allergens_count = filtered_recipes['Allergens'].value_counts(normalize=True) * 100
+    # Filter recipes that contain the query in the title
+    filtered_recipes = df[df['name'].str.contains(query, case=False, na=False)]
 
-    # Convert to JSON-friendly format (e.g., a list of dictionaries)
-    results = [{
-        'recipe_name': row['Food Product'],
-        'allergens': row['Allergens'],
-        'percentage': allergens_count.get(row['Allergens'], 0)
-    } for index, row in filtered_recipes.iterrows()]
+    # Calculate the percentage of products containing the allergen
+    total_products = len(filtered_recipes)
+    allergen_products_count = len(filtered_recipes[filtered_recipes['Allergens'].str.contains(allergen_to_check, case=False, na=False)])
+    percentage = (allergen_products_count / total_products) * 100 if total_products > 0 else 0
 
-    response = jsonify(results)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    # Get up to 10 non-allergen recipes
+    non_allergen_recipes = filtered_recipes[~filtered_recipes['Allergens'].str.contains(allergen_to_check, case=False, na=False)]
+    non_allergen_recipes_list = non_allergen_recipes.head(10).to_dict('records')  # Convert to list of dicts
+
+    # Construct response
+    response = {
+        'query': query,
+        'allergen': allergen_to_check,
+        'percentage': percentage,
+        'non_allergen_recipes': non_allergen_recipes_list
+    }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
